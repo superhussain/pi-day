08a41c5c376ef971f64336e0c644a74e25c741f1
Hussain Abbas <hussain@hussainabbas.com>
2016-03-15 20:32:46 -0400

    fix stylus in gulp script

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var uglify = require('gulp-uglify'),
     stylus = require('gulp-stylus'),
     livereload = require('gulp-livereload'),
     imagemin = require('gulp-imagemin'),
-    prefix = require('gulp-autoprefixer'),
+    autoprefixer = require('autoprefixer-stylus'),
     serve = require('gulp-webserver');
 
 // error log
@@ -26,14 +26,12 @@ gulp.task('scripts', function() {
 
 // styles task
 gulp.task('styles', function() {
-  // return stylus('styl/*.styl')
-  // .pipe(stylus({
-  //   compress: true
-  // }))
-  gulp.src('css/*.css')
+  return gulp.src('./styl/*.styl')
+  .pipe(stylus({
+		use: [autoprefixer('last 7 versions')]
+  }))
   .on('error', errorLog)
-  .pipe(prefix('last 7 versions'))
-  .pipe(gulp.dest('css'))
+  .pipe(gulp.dest('./css'))
   .pipe(livereload());
 });
 
